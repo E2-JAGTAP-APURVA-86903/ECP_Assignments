c1b2bd40633a74cf0065bd4773e54b0f9971768e
sunbeam <japurva734@gmail.com>
2024-09-06 23:49:11 +0530

    assign4 q7,q22

---
+++ assign-4-22.c
@@ -0,0 +1,60 @@
+#include<stdio.h>
+int main(){
+int i,j;
+/*int row=5;
+for(i=1;i<=row;i++)
+{
+for(int j=1;j<=i;j++)
+{
+printf(" *");
+}
+printf("\n");
+}*/
+
+/*int row=4;
+for(i=row;i>=1;i--)
+{
+for(j=i;j>=1;j--)
+{
+printf(" *");
+}
+printf("\n");
+}*/
+
+for(i=1;i<=5;i++)
+
+{
+printf("%d",i);
+for(j=1;j<=i;j++)
+{
+//printf("%d",i);
+}
+printf("\n");
+}
+return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
